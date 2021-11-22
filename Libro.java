afa824df3dfc9e2b675d4061bcae3c7c433385e2
Ivxn <gonzalez.martino.ivan@gmail.com>
2021-11-22 14:14:08 +0100

    1847.03-Atributo numero de paginas

--- Libro.java
+++ Libro.java
@@ -12,14 +12,16 @@ public class Libro {
 
     private String autor;
     private String titulo;
+    private int numeroPaginas;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro)
+    public Libro(String autorLibro, String tituloLibro, int paginasTotales)
     {
         autor = autorLibro;
         titulo = tituloLibro;
+        numeroPaginas = paginasTotales;
     }
     
     public String getAutor() {
@@ -36,5 +38,9 @@ public class Libro {
     
     public void imprimeTitulo() {
         System.out.println(titulo);
+    }
+    
+    public int getPaginas() {
+        return numeroPaginas;
     }
 }
